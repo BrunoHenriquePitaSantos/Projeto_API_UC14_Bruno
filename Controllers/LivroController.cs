665d5828fb92f09d82c445c24728637a4af95462
Bruno Santos <117130020+BrunoHenriquePitaSantos@users.noreply.github.com>
2023-09-24 18:53:43 -0300

    Projeto_API_UC14

---
+++ Controllers/LivroController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Projeto_API_19_09.Repositories;
+using Projeto_API_19_09.Models;
+
+namespace Projeto_API_19_09.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LivroController : ControllerBase
+    {
+        private readonly LivroRepository _livroRepository;
+
+        public LivroController(LivroRepository livroRepository)
+        {
+            _livroRepository = livroRepository;
+        }
+        [HttpGet]
+        public IActionResult Listar()
+        {
+            try
+            {
+                return Ok(_livroRepository.Listar());
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+    }
+}
